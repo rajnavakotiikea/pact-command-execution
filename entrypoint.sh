ce9e71a155266017704b451d2b0b0fdf73ea24a3
rajnavakotiikea <raj.navakoti@ingka.ikea.com>
2022-05-25 17:00:22 +0200

    updating shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,4 +21,13 @@ create_pact_webhook() {
 
 testing
 
-create_pact_webhook
+docker run --rm pactfoundation/pact-cli:latest broker create-webhook 'https://api.github.com/repos/rajnavakotiikea/example-provider/dispatches' \
+                    --header 'Content-Type: application/json' 'Accept: application/vnd.github.everest-preview+json' \
+                    "'Authorization: Bearer ${INPUT_GITHUB_TOKEN}'" \
+                    --request POST \
+                    --data '{ "event_type": "pact_changed", "client_payload": { "pact_url": "${pactbroker.pactUrl}" } }' \
+                    --provider pactflow-example-provider \
+                    --contract-content-changed \
+                    --description "Pact content changed for pactflow-example-provider" \
+                    --broker-base-url https://sampleautoamtiontestraj.pactflow.io \
+                    --broker-token GglUlzHa8Egn_fpkhzZQLw
